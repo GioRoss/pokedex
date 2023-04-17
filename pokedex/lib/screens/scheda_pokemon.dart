3ac8fc72bec24758da92780601246c8407015178
GioRoss <roselli.giorgia1@gmail.com>
2023-04-17 18:29:00 +0200

    aggiunto safeArea

--- pokedex/lib/screens/scheda_pokemon.dart
+++ pokedex/lib/screens/scheda_pokemon.dart
@@ -34,152 +34,154 @@ class _SchedaPokemonState extends State<SchedaPokemon> {
 
     return Scaffold(
       backgroundColor: pokemonColore,
-      body: Stack(
-        alignment: Alignment.center,
-        children: [
-          Positioned(
-            top: width > 1200 ? 1 : 3,
-            left: 9,
-            child: IconButton(
-              icon: Icon(
-                Icons.arrow_circle_left_outlined,
-                color: Colors.white,
-                size: width > 1200 ? 45 : 40,
-              ),
-              onPressed: () => Navigator.pop(context),
-            ),
-          ),
-          Positioned(
-            top: width > 1200 ? 55 : 60,
-            left: 20,
-            right: 20,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  pokemonNome,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: width > 1200
-                        ? 40
-                        : width < 310
-                            ? 20
-                            : 30,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  textAlign: TextAlign.left,
+      body: SafeArea(
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Positioned(
+              top: width > 1200 ? 1 : 3,
+              left: 9,
+              child: IconButton(
+                icon: Icon(
+                  Icons.arrow_circle_left_outlined,
+                  color: Colors.white,
+                  size: width > 1200 ? 45 : 40,
                 ),
-                Text(
-                  "#$pokemonId",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: width > 1200
-                        ? 40
-                        : width < 310
-                            ? 20
-                            : 30,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  textAlign: TextAlign.left,
-                ),
-              ],
-            ),
-          ),
-          Positioned(
-            top: 105,
-            left: 22,
-            child: Wrap(
-              direction: Axis.vertical,
-              spacing: 5,
-              children: [
-                ...pokemonData.getPokemonType(
-                    pokemonTipo, width > 310 ? width : width / 3),
-              ],
-            ),
-          ),
-          Positioned(
-            bottom: (height / 2) + 10,
-            right: -30,
-            child: Image.asset(
-              '../../assets/images/pokeball.png',
-              height: width > 1200
-                  ? 250
-                  : width < 400
-                      ? 150
-                      : 200,
-              fit: BoxFit.fitHeight,
-            ),
-          ),
-          Positioned(
-            bottom: 0,
-            child: Container(
-              width: width,
-              height: height * 0.53,
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topRight: Radius.circular(20),
-                  topLeft: Radius.circular(20),
-                ),
-                color: Colors.white,
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(20),
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: <Widget>[
-                      const SizedBox(height: 30.0),
-                      InfoSchedaPokemon(
-                        width: width,
-                        etichetta: 'Nome: ',
-                        descrizione: pokemonNome,
-                      ),
-                      InfoSchedaPokemon(
-                        width: width,
-                        etichetta: 'Altezza: ',
-                        descrizione: '${pokemonAltezza.toString()}.0 cm',
-                      ),
-                      InfoSchedaPokemon(
-                        width: width,
-                        etichetta: 'Peso: ',
-                        descrizione: '${pokemonPeso.toString()}.0 gr',
-                      ),
-                      InfoSchedaPokemonLista(
-                        width: width,
-                        pokemonListInfo: pokemonAbilita,
-                        pokemonData: pokemonData,
-                        etichetta: 'Abilità: ',
-                      ),
-                      InfoSchedaPokemonLista(
-                        width: width,
-                        pokemonListInfo: pokemonMosse,
-                        pokemonData: pokemonData,
-                        etichetta: 'Mosse: ',
-                      ),
-                      InfoSchedaPokemon(
-                        width: width,
-                        etichetta: 'Esperienza Iniziale: ',
-                        descrizione: pokemonEsperienza.toString(),
-                      ),
-                    ],
-                  ),
-                ),
+                onPressed: () => Navigator.pop(context),
               ),
             ),
-          ),
-          Positioned(
-            bottom: (height / 2) - 5,
-            left: width < 350 ? width / 3 : (width / 2) - 10,
-            child: Image.network(
-              pokemonImg,
-              height: width > 1200
-                  ? 250
-                  : width < 450
-                      ? 150
-                      : 200,
-              fit: BoxFit.cover,
+            Positioned(
+              top: width > 1200 ? 55 : 60,
+              left: 20,
+              right: 20,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    pokemonNome,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: width > 1200
+                          ? 40
+                          : width < 310
+                              ? 20
+                              : 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.left,
+                  ),
+                  Text(
+                    "#$pokemonId",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: width > 1200
+                          ? 40
+                          : width < 310
+                              ? 20
+                              : 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.left,
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+            Positioned(
+              top: 105,
+              left: 22,
+              child: Wrap(
+                direction: Axis.vertical,
+                spacing: 5,
+                children: [
+                  ...pokemonData.getPokemonType(
+                      pokemonTipo, width > 310 ? width : width / 3),
+                ],
+              ),
+            ),
+            Positioned(
+              bottom: (height / 2) + 10,
+              right: -30,
+              child: Image.asset(
+                '../../assets/images/pokeball.png',
+                height: width > 1200
+                    ? 250
+                    : width < 400
+                        ? 150
+                        : 200,
+                fit: BoxFit.fitHeight,
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              child: Container(
+                width: width,
+                height: height * 0.53,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(20),
+                    topLeft: Radius.circular(20),
+                  ),
+                  color: Colors.white,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: <Widget>[
+                        const SizedBox(height: 30.0),
+                        InfoSchedaPokemon(
+                          width: width,
+                          etichetta: 'Nome: ',
+                          descrizione: pokemonNome,
+                        ),
+                        InfoSchedaPokemon(
+                          width: width,
+                          etichetta: 'Altezza: ',
+                          descrizione: '${pokemonAltezza.toString()}.0 cm',
+                        ),
+                        InfoSchedaPokemon(
+                          width: width,
+                          etichetta: 'Peso: ',
+                          descrizione: '${pokemonPeso.toString()}.0 gr',
+                        ),
+                        InfoSchedaPokemonLista(
+                          width: width,
+                          pokemonListInfo: pokemonAbilita,
+                          pokemonData: pokemonData,
+                          etichetta: 'Abilità: ',
+                        ),
+                        InfoSchedaPokemonLista(
+                          width: width,
+                          pokemonListInfo: pokemonMosse,
+                          pokemonData: pokemonData,
+                          etichetta: 'Mosse: ',
+                        ),
+                        InfoSchedaPokemon(
+                          width: width,
+                          etichetta: 'Esperienza Iniziale: ',
+                          descrizione: pokemonEsperienza.toString(),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: (height / 2) - 5,
+              left: width < 350 ? width / 3 : (width / 2) - 10,
+              child: Image.network(
+                pokemonImg,
+                height: width > 1200
+                    ? 250
+                    : width < 450
+                        ? 150
+                        : 200,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
